2d109913ad71e301819dd1a73879ed9c76808aa2
YujeongLEE <dldbwjd24@gmail.com>
2026-07-13 12:17:54 +1000

    fix : change pages into demo version

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,21 @@
-import React from 'react';
-import {Routes, Route, BrowserRouter} from "react-router-dom";
+import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 //pages
-import AuthPage from './pages/authPage/index';
-import ErrorPage from './pages/errorPage/index';
-import ProfilePage from './pages/profilePage/index';
-import MainPage from './pages/mainPage/index';
-import EditPage from './pages/editPage';
-
-
+import ErrorPage from "./pages/errorPage/index";
+import MainPage from "./pages/mainPage/index";
+import EditPage from "./pages/editPage";
 
 function App() {
-
-
-
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<MainPage/>}></Route>
-          <Route path="/edit" element={<EditPage/>}></Route>
-          <Route path="/auth" element={<AuthPage/>}></Route>
-          <Route path="/profile" element={<ProfilePage/>}></Route>
-          <Route path="/error" element={<ErrorPage/>}></Route>
+          <Route path="/" element={<MainPage />}></Route>
+          <Route path="/edit" element={<EditPage />}></Route>
+          <Route path="/error" element={<ErrorPage />}></Route>
         </Routes>
-      </BrowserRouter>      
+      </BrowserRouter>
     </div>
   );
 }
